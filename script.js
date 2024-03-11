77d2411f6d997ccc02a63a6ae614c91496fca0d6
AliAhmed205 <Ali_lazuli@outlook.com>
2024-03-11 11:10:09 +0100

    Refactoring HTML/CSS with new additional SVGs

--- script.js
+++
@@ -1,7 +0,0 @@
-function applyRandomColor() {
-    var randomIndex = Math.floor(Math.random() * 3) + 1; // Generate a random number between 1 and 3
-    var img = document.querySelector('/randomImage');
-    img.className = 'random-color-' + randomIndex; 
-  
-  applyRandomColor();
-}
